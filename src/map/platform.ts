28b15a7463d377551bbed52c492966d1369e8adf
Michal Zalecki <michal@michalzalecki.com>
2017-06-11 12:33:55 +0200

    Use HTTPS mode with HERE

--- src/map/platform.ts
+++ src/map/platform.ts
@@ -1,8 +1,9 @@
 declare const H: any;
 
 const platform = new H.service.Platform({
-  'app_id': process.env.HERE_APP_ID,
-  'app_code': process.env.HERE_APP_CODE,
+  app_id: process.env.HERE_APP_ID,
+  app_code: process.env.HERE_APP_CODE,
+  useHTTPS: true
 });
 
 export default platform;
